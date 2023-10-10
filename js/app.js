10add4de017a11f4bac40bac94ada1394fa39a09
godfred-adams <adamsgodfred3.tech@gmail.com>
2023-10-10 16:46:27 +0000

    added all functionalities to the buttons

---
+++ js/app.js
@@ -0,0 +1,52 @@
+const buttonElement = document.querySelectorAll('.js-button');
+const screenElement = document.querySelector('.screen-number');
+
+let calculation = JSON.parse(localStorage.getItem('calculation')) || '';
+
+displayCalculation();
+
+//adding event listeners to the buttons
+buttonElement.forEach((button) => {
+  button.addEventListener('click', () => {
+    calculation += button.getAttribute('data-set');
+    saveCalculation();
+    displayCalculation();
+  }); 
+});
+
+document.querySelector('.js-equal-button')
+  .addEventListener('click', () => {
+    calculation = eval(calculation);
+    saveCalculation();
+    displayCalculation();
+  }
+);
+
+document.querySelector('.js-del-button')
+  .addEventListener('click', () => {
+    calculation = calculation.substring(0,calculation.length -1);
+    displayCalculation();
+  });
+
+document.querySelector('.js-reset-button')
+  .addEventListener('click', () => {
+    calculation = '';
+    localStorage.removeItem('calculation');
+    displayCalculation();
+  });
+
+
+
+  // function for displaying the calculation on screen
+
+  function displayCalculation() {
+    if (calculation === '') {
+      screenElement.innerHTML = `<p>0</p>`;
+    } else {
+      screenElement.innerHTML = `${calculation}`;
+    }
+  }
+
+  function saveCalculation() {
+    localStorage.setItem('calculation', JSON.stringify(calculation));
+  }
